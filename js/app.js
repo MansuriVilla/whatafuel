f8811c4ba7c495e77487fb078b5fa0088b5c9a93
MansuriVilla <mansuriadnan2004@gmail.com>
2025-04-27 15:08:24 +0530

    Made Some Changes for Laptop

--- js/app.js
+++ js/app.js
@@ -228,6 +228,20 @@ velocitySlider();
 });
 
 var swiper = new Swiper(".journal_slider", {
-  slidesPerView: 3.5,
+  // slidesPerView: 3.5,
   spaceBetween: 30,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 3.5,
+      spaceBetween: 30,
+    },
+  },
 });
